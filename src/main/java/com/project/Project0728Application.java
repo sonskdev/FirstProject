91cbf87091ac99745858e717b6b2d0734d29b6d8
son.sk.dev <son.sk.dev@ivycomtech.com>
2022-07-28 16:20:32 +0900

    init Commit

---
+++ src/main/java/com/project/Project0728Application.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project0728Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project0728Application.class, args);
+	}
+
+}
